b29e9bed5d7a6bedce353f4a08ea1ef964db8ba8
CodingAndTechnology <CodingAndTechnology@aol.com>
2023-12-18 18:04:18 +0530

    added an example of create and destroy memory

---
+++ zigcode/create_destroy.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn main() !void{
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	defer _ = gpa.deinit();
+	
+	const allocator = gpa.allocator();
+	var book = try allocator.create(Book);
+	defer allocator.destroy(book);
+	
+	book.title="PHP";
+	book.price=23.3;
+	book.pages=350;
+	
+	std.debug.print("{any}",.{book});
+}
+
+const Book = struct{
+	title:[]const u8,
+	price:f32,
+	pages:u32
+};
