517681dbc12ab4234beb3992587bbd085c6fb2be
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-03-27 21:23:46 +0530

    added http downlad example

---
+++ zigcode/download_file.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const http = std.http;
+const print = std.debug.print;
+
+pub fn main() !void{
+	const uri = try std.Uri.parse("https://www.rust-lang.org/logos/rust-logo-512x512.png");
+	var segments = std.mem.splitBackwardsSequence(u8, uri.path, "/");	
+	const file_name = segments.first();	
+	
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+	
+	var client = http.Client{ .allocator = allocator };
+    defer client.deinit();
+	
+	
+	var buf: [8096]u8 = undefined;
+	const reqOpt = http.Client.RequestOptions{ .server_header_buffer = &buf };
+    var req = try client.open(.GET, uri, reqOpt);
+    defer req.deinit();
+    try req.send(.{});
+
+    try req.wait();
+    if (req.response.status == .ok) {
+        print("Response Status:{}\n", .{req.response.status});
+        
+        var rdr = req.reader();
+        const content = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
+        defer allocator.free(content);
+
+        const file = try std.fs.cwd().createFile(file_name,.{},);
+		defer file.close();
+
+		try file.writeAll(content);
+    }
+}
