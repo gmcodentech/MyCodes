e69e325737def38dc1ca15cd77939df91aeb2c92
gmcodentech <144211927+gmcodentech@users.noreply.github.com>
2024-04-06 21:16:06 +0530

    Delete zigcode/call_c/zig-cache directory

--- zigcode/call_c/zig-cache/o/9c9cc5d88bbcc1531961874cc722eef9/cimport.h.d
+++
@@ -1,4 +0,0 @@
-cimport.o: \
-  C:/Software/Zig/Repo/MyCodes/zigcode/call_c/zig-cache/o/9c9cc5d88bbcc1531961874cc722eef9/cimport.h \
-  C:/Software/Zig/Repo/MyCodes/zigcode/call_c/c_src/math.c \
-  C:/Software/Zig/Repo/MyCodes/zigcode/call_c/c_src/math.h
--- zigcode/call_c/zig-cache/o/b5a33ce76a19fc496db46add6a7ee59a/cimport.h.d
+++
@@ -1,3 +0,0 @@
-cimport.o: \
-  C:/Software/Zig/Repo/MyCodes/zigcode/call_c/zig-cache/o/b5a33ce76a19fc496db46add6a7ee59a/cimport.h \
-  C:/Software/Zig/Repo/MyCodes/zigcode/call_c/c_src/math.h
