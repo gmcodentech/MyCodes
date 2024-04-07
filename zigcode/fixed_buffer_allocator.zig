217925ac04e31745d30f6c25078e29d1414af3d6
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-04-07 20:54:02 +0530

    more zig examples

---
+++ zigcode/fixed_buffer_allocator.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn main() !void{
+	var buf:[100]u8 = undefined;
+	var fa =std.heap.FixedBufferAllocator.init(&buf);
+	defer fa.reset();
+	
+	const allocator =fa.allocator();
+	
+	const str = try std.fmt.allocPrint(allocator,"The salary is {d}",.{23});
+	defer allocator.free(str);
+	
+	std.debug.print("{s}",.{str});
+}
