ecede5e142158652a7f766499454b64693d82332
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-04-07 21:05:01 +0530

    formating applied

--- zigcode/max_min.zig
+++ zigcode/max_min.zig
@@ -1,11 +1,11 @@
 //max_min.zig
 const std = @import("std");
-pub fn main() void{
-	const max_no = @max(3,4,34,32,35,6);
-	std.debug.print("maximum no is {d}",.{max_no});
-	
-	const min_no = @min(3,4,34,32,35,6);
-	std.debug.print("\nminimum no is {d}",.{min_no});
+pub fn main() void {
+    const max_no = @max(3, 4, 34, 32, 35, 6);
+    std.debug.print("maximum no is {d}", .{max_no});
+
+    const min_no = @min(3, 4, 34, 32, 35, 6);
+    std.debug.print("\nminimum no is {d}", .{min_no});
 }
 
 //output:
