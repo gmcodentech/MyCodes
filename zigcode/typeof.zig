c5e2a643b0038ac25e559e60a1c462bb2425fae4
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-04-16 19:32:08 +0530

    code changed

--- zigcode/typeof.zig
+++ zigcode/typeof.zig
@@ -1,10 +1,7 @@
-const std = @import("std");
-
-pub fn main() void {
-	const type_of_float = @TypeOf(f32);
-	if (type_of_float == @TypeOf(f32)){
-		std.debug.print("it's float",.{});
-	}else {
-		std.debug.print("it's not a float",.{});
-	}	
-}
+   const std = @import("std");
+   
+   pub fn main() void {
+   	const f:f32 = 2.3;
+   	std.debug.print("{any}\n",.{@TypeOf(f)==f32});
+   	std.debug.print("{any}",.{f32});
+   }
