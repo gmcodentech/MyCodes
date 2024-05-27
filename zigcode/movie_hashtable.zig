6f4e4dd115bf692186aa77cc0d068718b790d828
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-05-27 15:47:11 +0530

    new examples

--- zigcode/movie_hashtable.zig
+++ zigcode/movie_hashtable.zig
@@ -1,15 +1,48 @@
 const std = @import("std");
+const fs = std.fs;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 	
-	var lines = try allocator.alloc([]const u8,3);
-	defer allocator.free(lines);
-	lines[0]="Milk,44.5,300,1";
-	lines[1]="Sugar,53.3,90,0";
-	lines[2]="Tea,5.3,190,1";
+	const file = try fs.cwd().openFile("C:/Software/Dotnet/Data/short.tsv", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 0;
+	const record_size = 1941;
 	
+	var lines = try allocator.alloc([]const u8,record_size);
+	defer {
+		for (lines) |l|{
+			allocator.free(l);
+		}
+		allocator.free(lines);
+	}
+	
+    while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
+		
+        defer line.clearRetainingCapacity();
+		const trimmed = std.mem.trimRight(u8, line.items, "\r");
+		
+		lines[line_no] = try allocator.dupe(u8, trimmed);
+		if(line_no == record_size-1){
+			break;
+		}
+    } else |err| switch (err) {
+        error.EndOfStream => {}, // Continue on
+        else => return err, // Propagate error
+    }
+
+	// for (lines) |l|{
+		// std.debug.print("{s}\n",.{l});
+	// }
 	
 	var json_map = std.StringHashMap([]const u8).init(allocator);
 	defer {
@@ -26,7 +59,7 @@ pub fn main() !void {
 	}
 	
 	for (0..lines.len) |c|{
-		const p1 = try getJson(Product, allocator, lines[c],",");
+		const p1 = try getJson(Movie, allocator, lines[c],"\t");
 		var buf: [10]u8 = undefined;
 		const str = try std.fmt.bufPrint(&buf, "{d}", .{c+1});
 		try json_map.put(try allocator.dupe(u8,str), p1);
@@ -89,8 +122,8 @@ fn getJson(comptime T: type, allocator: std.mem.Allocator, line: []const u8,sepa
     var it = std.mem.split(u8, line, separator);
     var i: usize = 0;
     while (it.next()) |p| {
-        tokens[i] = p;
-        i += 1;
+		tokens[i] = p;
+		i += 1;
     }
     i = 0;
     inline for (st.fields) |field| {
@@ -103,7 +136,6 @@ fn getJson(comptime T: type, allocator: std.mem.Allocator, line: []const u8,sepa
 }
 
 fn getValue(comptime data_type: type, value: []const u8) !data_type {
-
 	return switch(@typeInfo(data_type)){
 		.Int => try std.fmt.parseInt(data_type, value, 10),
 		.Float => try std.fmt.parseFloat(data_type, value),
@@ -113,9 +145,15 @@ fn getValue(comptime data_type: type, value: []const u8) !data_type {
 	};
 }
 
-const Product = struct {
-    name: []const u8,
-    price: f32,
-    units: u32,
-	in_stock:[]const u8,
+
+const Movie = struct {
+    tconst: []const u8,
+    title_type: []const u8,
+    primary_title: []const u8,
+	original_title: []const u8,
+    isadult: u32,
+    start_year: u32,
+	end_year: u32,
+    length: u32,
+	genres: []const u8,
 };
