2172da935d5c0e0519bcc0e0715722b4fd3e68b2
CodingAndTechnology <CodingAndTechnology@aol.com>
2024-05-27 15:47:11 +0530

    new examples

--- zigcode/read_lines_file.zig
+++ zigcode/read_lines_file.zig
@@ -9,11 +9,9 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file = try fs.cwd().openFile("C:/Software/Dotnet/Data/imdb.tsv", .{});
+    const file = try fs.cwd().openFile("C:/Software/Dotnet/Data/short.tsv", .{});
     defer file.close();
-//Test comment
-    // Wrap the file reader in a buffered reader.
-    // Since it's usually faster to read a bunch of bytes at once.
+
     var buf_reader = std.io.bufferedReader(file.reader());
     const reader = buf_reader.reader();
 
@@ -21,58 +19,28 @@ pub fn main() !void {
     defer line.deinit();
 
     const writer = line.writer();
-    var line_no: usize = 1;
+    var line_no: usize = 0;
 	
-	var movies = std.ArrayList(*Movie).init(allocator);
-	defer movies.deinit();
-	
+	var lines = try allocator.alloc([]const u8,23);
+	defer {
+		for (lines) |l|{
+			allocator.free(l);
+		}
+		allocator.free(lines);
+	}
     while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
-        // Clear the line so we can reuse it.
         defer line.clearRetainingCapacity();
-		const movie = try getMovie(allocator,line.items);
-		defer allocator.destroy(movie);
-		try movies.append(movie);
-        //print("{d}--{s}\n", .{ line_no, movie.tconst });
-		if(line_no == 3){
+		const str = try allocator.dupe(u8,line.items);	
+		lines[line_no] = str;
+		if(line_no == 22){
 			break;
 		}
     } else |err| switch (err) {
         error.EndOfStream => {}, // Continue on
         else => return err, // Propagate error
     }
-	
-	for (movies.items) |movie|{
-		std.debug.print("{s}\n",.{movie.tconst});
+
+	for (lines) |l|{
+		std.debug.print("{s}\n",.{l});
 	}
-	
-	// for (movies.items) |*movie|{
-		// allocator.destroy(movie.*);
-	// }
 }
-
-fn getMovie(allocator:std.mem.Allocator,line:[]const u8) !*Movie{
-	var it = std.mem.split(u8, line, "\t");
-	const movie = try allocator.create(Movie);
-	movie.* = Movie{
-		.tconst = if(it.next()) |x| x else "",
-		.title_type = if(it.next()) |x| x else "",
-		.primary_title = if(it.next()) |x| x else "",
-		.original_title = if(it.next()) |x| x else "",
-		.isadult = if(it.next()) |x| try std.fmt.parseInt(i32,x,10) else 0,
-		.year = if(it.next()) |x| try std.fmt.parseInt(i32,x,10) else 0,
-		.length = if(it.next()) |x| try std.fmt.parseInt(i32,x,10) else 0,
-		.genres = if(it.next()) |x| x else "",
-	};
-    return movie;
-}
-
-const Movie = struct {
-    tconst: []const u8,
-    title_type: []const u8,
-    primary_title: []const u8,
-	original_title: []const u8,
-    isadult: i32,
-    year: ?i32,
-    length: ?i32,
-	genres: []const u8,
-};
