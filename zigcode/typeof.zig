1e82a2976c6c841463dea7a20095c82c5ea004cc
CodingAndTechnology <CodingAndTechnology@aol.com>
2025-06-01 20:26:53 +0530

    new examples

--- zigcode/typeof.zig
+++ zigcode/typeof.zig
@@ -1,7 +1,7 @@
-   const std = @import("std");
+const std = @import("std");
    
-   pub fn main() void {
+pub fn main() void {
    	const f:f32 = 2.3;
    	std.debug.print("{any}\n",.{@TypeOf(f)==f32});
    	std.debug.print("{any}",.{f32});
-   }
+}
