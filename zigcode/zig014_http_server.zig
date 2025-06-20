a51540ddd5e2057a8d622176fd8a01e9656fc766
CodingAndTechnology <CodingAndTechnology@aol.com>
2025-06-21 05:26:37 +0530

    some code formatting

--- zigcode/zig014_http_server.zig
+++ zigcode/zig014_http_server.zig
@@ -1,31 +1,31 @@
 const std = @import("std");
 const print = std.debug.print;
 pub fn main() !void {
-	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-	defer _ = gpa.deinit();
-	const allocator = gpa.allocator();
-	
-	const testPort = std.process.getEnvVarOwned(allocator,"TEST_PORT") catch |err| {
-		print("Returned an error now: {any}",.{err});
-		return;
-	};
-	errdefer allocator.free(testPort);
-	defer allocator.free(testPort);
-	
-	print("Server started listening on {s}",.{testPort});
-    const address = try std.net.Address.parseIp4("0.0.0.0", try std.fmt.parseInt(u16,testPort,10));
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const testPort = std.process.getEnvVarOwned(allocator, "TEST_PORT") catch |err| {
+        print("Returned an error now: {any}", .{err});
+        return;
+    };
+    errdefer allocator.free(testPort);
+    defer allocator.free(testPort);
+
+    print("Server started listening on {s}", .{testPort});
+    const address = try std.net.Address.parseIp4("0.0.0.0", try std.fmt.parseInt(u16, testPort, 10));
 
     var server = try address.listen(std.net.Address.ListenOptions{});
     defer server.deinit();
 
-	while(true){
-	const server_thread = try std.Thread.spawn(.{}, (struct {
-        fn apply(s: *std.net.Server) !void {
-			try handleConnection(try s.accept());
-		}
-	}).apply, .{&server});
-	server_thread.join();
-	}
+    while (true) {
+        const server_thread = try std.Thread.spawn(.{}, (struct {
+            fn apply(s: *std.net.Server) !void {
+                try handleConnection(try s.accept());
+            }
+        }).apply, .{&server});
+        server_thread.join();
+    }
 }
 
 fn handleConnection(conn: std.net.Server.Connection) !void {
