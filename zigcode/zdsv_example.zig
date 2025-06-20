1f0ead71f157c9883f35bb6e789e6ac87432f618
CodingAndTechnology <CodingAndTechnology@aol.com>
2025-06-21 05:26:37 +0530

    some code formatting

--- zigcode/zdsv_example.zig
+++ zigcode/zdsv_example.zig
@@ -3,11 +3,11 @@ const print = std.debug.print;
 const ZDSV = @import("zdsv.zig").ZDSV;
 
 pub fn main() !void {
-	var dbg = std.heap.DebugAllocator(.{}){};
-	defer _ = dbg.deinit();
-	const allocator = dbg.allocator();
+    var dbg = std.heap.DebugAllocator(.{}){};
+    defer _ = dbg.deinit();
+    const allocator = dbg.allocator();
     const zdsv = try ZDSV(User).init(allocator);
-	defer zdsv.deinit();
+    defer zdsv.deinit();
     const users = try zdsv.get("users.csv", ",", false, 10);
     for (users.items) |*user| {
         print("{d} {s} {d}\n", .{ user.id, user.name, user.age });
