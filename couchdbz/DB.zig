e7fedf41b2adc4dfc70b98873c64731ad80b1091
CodingAndTechnology <CodingAndTechnology@aol.com>
2025-07-24 19:38:55 +0530

    more code for couchdb zig

--- couchdbz/DB.zig
+++ couchdbz/DB.zig
@@ -19,8 +19,8 @@ pub fn DB(comptime T: type) type {
             id: []const u8,
             key: []const u8,
             value: Value,
-            doc: T,
-        };
+            doc: ?T,
+        }; 
 
         const CouchDBResponse = struct {
             total_rows: u32,
@@ -39,14 +39,15 @@ pub fn DB(comptime T: type) type {
 			bookmark:[]u8,
 		};
 		
-
+		
 
         const UUIDResponse = struct { uuids: [][]u8 };
 		
         pub fn init(allocator: std.mem.Allocator, database_name: []const u8,username:[]const u8,password:[]const u8) Self {
             arena = std.heap.ArenaAllocator.init(allocator);
             const arenaAllocator = arena.allocator();
-            const http_client = HttpClient.init(arenaAllocator, username, password);
+            
+			const http_client = HttpClient.init(arenaAllocator, username, password);
             return Self{ .allocator = arenaAllocator, .database_name = database_name, .client = http_client };
         }
         pub fn deinit(self: *Self) void {
@@ -54,16 +55,29 @@ pub fn DB(comptime T: type) type {
             arena.deinit();
         }
 		
+		pub fn getDbMetadata(self:*Self) !Info{
+			const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ self.base_url, self.database_name });
+			defer  self.allocator.free(url);
+			
+            const metadata_json = try self.client.send(.GET, url, "");
+			defer self.allocator.free(metadata_json);
+			
+			const parsed_metadata = try std.json.parseFromSlice(Info, self.allocator, metadata_json, .{ .ignore_unknown_fields = true });
+            return parsed_metadata.value;
+		}
+		
 		pub fn createDb(self:*Self) !bool{
 			const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ self.base_url, self.database_name });
             defer self.allocator.free(url);
-            _ = try self.client.send(.PUT, url, "");
+            
+			_ = try self.client.send(.PUT, url, "");
 			return true;
 		}
 		
 		pub fn checkForDb(self:*Self,db:[]const u8) !bool{
 			const url = try std.fmt.allocPrint(self.allocator, "{s}/_all_dbs", .{ self.base_url });
             defer self.allocator.free(url);
+			
 			const resp_json = try self.client.send(.GET, url, "");
             const db_list = try std.json.parseFromSlice([][]u8, self.allocator, resp_json, .{ .ignore_unknown_fields = true });
             defer self.allocator.free(resp_json);
@@ -82,18 +96,20 @@ pub fn DB(comptime T: type) type {
 
         fn save(self: *Self, url: []const u8, method: std.http.Method, json: []u8) !RecordCreateResponse {
             const response_json = try self.client.send(method, url, json);
+			defer self.allocator.free(response_json);
+			
             const response = try std.json.parseFromSlice(RecordCreateResponse, self.allocator, response_json, .{ .ignore_unknown_fields = true });
-            self.allocator.free(response_json);
             return response.value;
         }
 
         pub fn create(self: *Self, item: anytype) !RecordCreateResponse {
             const json = try std.json.stringifyAlloc(self.allocator, item, .{ .whitespace = .indent_2 });
+			defer self.allocator.free(json);
+			
             const uuid = try getUuid(self.allocator, &self.client, self.base_url);
-            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}", .{ self.base_url, self.database_name, uuid });
-
-            defer self.allocator.free(url);
-            defer self.allocator.free(json);
+            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}:{s}", .{ self.base_url, self.database_name,item.type, uuid });
+			defer self.allocator.free(url);            
+           
             return save(self, url, .PUT, json);
         }
 		
@@ -113,54 +129,88 @@ pub fn DB(comptime T: type) type {
 
         pub fn update(self: *Self, item: T) !RecordCreateResponse {
             const json = try std.json.stringifyAlloc(self.allocator, item, .{ .whitespace = .indent_2 });
-            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}", .{ self.base_url, self.database_name, item._id });
-
+			defer self.allocator.free(json);
+            
+			const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}", .{ self.base_url, self.database_name, item._id });
             defer self.allocator.free(url);
-            defer self.allocator.free(json);
-            return save(self, url, .PUT, json);
+            
+			return save(self, url, .PUT, json);
         }
         pub fn delete(self: *Self, item: T) !RecordCreateResponse {
             const json = try std.json.stringifyAlloc(self.allocator, item, .{ .whitespace = .indent_2 });
-            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}?rev={s}", .{ self.base_url, self.database_name, item._id, item._rev });
-
-            defer self.allocator.free(url);
             defer self.allocator.free(json);
-            return save(self, url, .DELETE, json);
+			
+			const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}?rev={s}", .{ self.base_url, self.database_name, item._id, item._rev });
+			defer self.allocator.free(url);
+            
+			return save(self, url, .DELETE, json);
         }
 
         fn getUuid(allocator: std.mem.Allocator, client: *HttpClient, base_url: []const u8) ![]u8 {
             const url = try std.fmt.allocPrint(allocator, "{s}/_uuids", .{base_url});
             defer allocator.free(url);
-            const uuids_json = try client.send(.GET, url, "");
+            
+			const uuids_json = try client.send(.GET, url, "");
             defer allocator.free(uuids_json);
-            const parsed_product = try std.json.parseFromSlice(UUIDResponse, allocator, uuids_json, .{ .ignore_unknown_fields = true });
+            
+			const parsed_product = try std.json.parseFromSlice(UUIDResponse, allocator, uuids_json, .{ .ignore_unknown_fields = true });
             return parsed_product.value.uuids[0];
         }
 
         pub fn getById(self: *Self, id: []u8) !T {
             const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/{s}", .{ self.base_url, self.database_name, id });
+			defer  self.allocator.free(url);
+			
             const product_json = try self.client.send(.GET, url, "");
-            const parsed_product = try std.json.parseFromSlice(T, self.allocator, product_json, .{ .ignore_unknown_fields = true });
-            self.allocator.free(url);
-            self.allocator.free(product_json);
+			defer self.allocator.free(product_json);
+			
+			const parsed_product = try std.json.parseFromSlice(T, self.allocator, product_json, .{ .ignore_unknown_fields = true });
             return parsed_product.value;
         }
 
-        pub fn getList(self: *Self) ![]T {
-            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/_all_docs?include_docs=true", .{ self.base_url, self.database_name });
-            const product_json = try self.client.send(.GET, url, "");
+        pub fn getList(self: *Self,count:u64) ![]T {
+            const url = try std.fmt.allocPrint(self.allocator, "{s}/{s}/_all_docs?include_docs=true&limit={d}", 
+			.{ self.base_url, self.database_name, count });
+            defer self.allocator.free(url);
+			
+  		    const product_json = try self.client.send(.GET, url, "");
             defer self.allocator.free(product_json);
 
             const parsed_product = try std.json.parseFromSlice(CouchDBResponse, self.allocator, product_json, .{ .ignore_unknown_fields = true });
             const response = parsed_product.value;
             var list = std.ArrayList(T).init(self.allocator);
             errdefer list.deinit();
-            for (response.rows) |row| {
-                try list.append(row.doc);
+            
+			for (response.rows) |row| {
+				if(row.doc) |d|{
+					try list.append(d);
+				}
             }
             const items = try list.toOwnedSlice();
-            self.allocator.free(url);
             return items;
         }
     };
 }
+
+pub const Info = struct {
+			instance_start_time: []const u8,
+			db_name: []const u8,
+			purge_seq: []const u8,
+			update_seq: []const u8,
+			sizes: struct {
+				file: usize,
+				external: usize,
+				active: usize,
+			},
+			props: std.json.Value,
+			doc_del_count: usize,
+			doc_count: usize,
+			disk_format_version: usize,
+			compact_running: bool,
+			cluster: struct {
+				q: usize,
+				n: usize,
+				w: usize,
+				r: usize,
+			},
+};
